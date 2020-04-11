7ee949757d20e9d7b5352c89c59e1861b0dcf35a
Michael Gobz <47690301+michaelgobz@users.noreply.github.com>
2020-04-11 13:57:19 +0300

    port_changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require ('path');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.port;
 
 app.use(express.static(path.join(__dirname, './public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
