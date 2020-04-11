6869f8ffcaa37800c19d14956a8a96e41f7f3f0b
Michael Gobz <47690301+michaelgobz@users.noreply.github.com>
2020-04-11 14:41:43 +0300

    performance_1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require ('path');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
