86363208f43e1e8ed429f108886b9ac2da1fda02
Michael Gobz <47690301+michaelgobz@users.noreply.github.com>
2020-04-11 21:33:17 +0300

    made_faster_1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require ('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/css', express.static(path.join(__dirname, './public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/', '/index.html'))
