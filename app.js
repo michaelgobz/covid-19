1047a890b576d862070007296e28aff59a79d095
Michael Gobz <47690301+michaelgobz@users.noreply.github.com>
2020-04-11 22:58:31 +0300

    gzip_2

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const path = require('path');
-const expressStaticGzip = require('express-static-gzip');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/css', express.static(path.join(__dirname, './public')));
-app.use('/', expressStaticGzip(path.join(__dirname), {
- enableBrotli: true
-}));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/', '/index.html'))
