a6000859cc0570c298a79c0d441f988c2c39e95e
Michael Gobz <47690301+michaelgobz@users.noreply.github.com>
2020-04-12 12:20:18 +0300

    compression_1

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-
+const compression = require('compression')
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/css', express.static(path.join(__dirname, './public')));
-
+app.use(compression())
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/', '/index.html'))
 });
